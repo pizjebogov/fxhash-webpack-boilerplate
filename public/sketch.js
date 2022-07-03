2db1bde5f119b00e4d1786a533e856fa541f1212
pizjebogov <dimadimadimavoenmeh@mail.ru>
2022-07-03 19:45:55 +0300

    Update sketch.js

--- public/sketch.js
+++ public/sketch.js
@@ -19,12 +19,12 @@ function setup() {
   sunText.push()
   sunText.translate(width/2,height/2)
   for(let i=0;i<minimal/4;i+=1){
-    for(let j=0;j<=360;j+=1){
+    for(let j=0;j<=360;j+=0.25){
       let x=i*cos(j)
       let y=i*sin(j)
       let r=random()
-      sunText.stroke(color(360,map(i,0,minimal/4,100,80),map(i,0,minimal/4,100,50)))
-      if(r<0.75){
+      sunText.stroke(color(360,map(random(),0,1,100,50),map(i,0,minimal/4,100,30)))
+      if(r<=0.9){
         sunText.point(x,y)
       }
     }
@@ -40,9 +40,24 @@ function doResize(){
 
 function draw() {
   clear()
-  background(255)
-  image(sunText,0,0)
+  background('wheat')
+  
 
+  
+  for(let i=0;i<250;i+=1){
+    stroke(color(200,0,map(i,0,250,0,50)))
+    let endX=map(i,0,250,width/2-minimal/4,width/2)
+    let begX=map(i,0,250,0,width/2)
+    scribble.scribbleLine(begX,height,endX,height/2)
+  }
+  for(let i=0;i<250;i+=1){
+    stroke(color(200,0,map(i,0,250,50,0)))
+    let endX=map(i,0,250,width/2,width/2+minimal/4)
+    let begX=map(i,0,250,width/2,width)
+    scribble.scribbleLine(begX,height,endX,height/2)
+  }
+
+  image(sunText,0,0)
   fxpreview()
 
 }
